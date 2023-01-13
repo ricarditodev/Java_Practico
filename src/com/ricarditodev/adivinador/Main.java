b9efe9e564a83fd26e02e8b841e5d09216854fc0
ricarditodev <ricardodavid012@gmail.com>
2023-01-13 01:43:58 -0400

    resolviendo ejercicio adivinador

--- src/com/ricarditodev/adivinador/Main.java
+++ src/com/ricarditodev/adivinador/Main.java
@@ -1,5 +1,32 @@
 package com.ricarditodev.adivinador;
 
-public class Main {
+import java.util.Scanner;
+import java.util.Random;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in); //lectura de entrada
+        Random numeros = new Random(); //clase para generar numeros aleatorios
+
+        //se genera un numero entre 1 y 100
+        int numeroSecret = numeros.nextInt(100) + 1;
+        int valorUsuario;
+        boolean acierto = false;
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.print("Intento " + i + " >> ");
+            valorUsuario = entrada.nextInt();
+            entrada.nextLine();
+
+            if (valorUsuario == numeroSecret) {
+                System.out.print("Acertaste");
+                acierto = true;
+                break;
+            }
+        }
+
+        if (!acierto) {
+            System.out.println("No acertaste, el numero secreto era: " + numeroSecret);
+        }
+    }
 }
