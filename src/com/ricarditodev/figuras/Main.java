503babd8a93138362a57f3d4db27996ea2099396
ricarditodev <ricardodavid012@gmail.com>
2023-01-13 19:55:23 -0400

    fix figuras

--- src/com/ricarditodev/figuras/Main.java
+++ src/com/ricarditodev/figuras/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
+        //se declaran variables
         double area;
         double perimetro;
         double largo;
@@ -16,71 +17,93 @@ public class Main {
         final double PI = 3.14159265358979323846;
         String opcion;
 
+        //le mostramos la informacion al usuario - debe introducir una opcion
         System.out.print("Escoge una opción: \n1 o a) Rectangulo \n2 o b) Circunferencia \n3 o c) Cuadrado \n4 o d) Triangulo \n0 o s) Para salir \n>> ");
-
         opcion = entrada.nextLine();
 
-        if (opcion.equals("1") || opcion.equals("a") || opcion.equals("A")) {
-            System.out.print("Ingresa el largo: ");
-            largo = entrada.nextDouble();
-            entrada.nextLine();
+        //mientras la opcion sea distinta de 0, s, S(salir)
+        //ejecutamos el programa indefinidamente
+        while (!opcion.equals("0") && !opcion.equals("s") && !opcion.equals("S")) {
+            if (opcion.equals("1") || opcion.equals("a") || opcion.equals("A")) {
+                System.out.print("Ingresa el largo: ");
+                largo = entrada.nextDouble();
+                entrada.nextLine();
 
-            System.out.print("Ingresa el ancho: ");
-            ancho = entrada.nextDouble();
-            entrada.nextLine();
+                System.out.print("Ingresa el ancho: ");
+                ancho = entrada.nextDouble();
+                entrada.nextLine();
 
-            perimetro = largo * 2 + ancho * 2;
-            area = largo * ancho;
+                perimetro = largo * 2 + ancho * 2;
+                area = largo * ancho;
 
-            System.out.println("Perimetro: " + perimetro + "\nArea: " + area);
-        } else if (opcion.equals("2") || opcion.equals("b") || opcion.equals("B")) {
-            System.out.print("Ingresa el radio: ");
-            radio = entrada.nextDouble();
-            entrada.nextLine();
+                System.out.println("Perimetro: " + perimetro + "\nArea: " + area);
 
-            perimetro = radio * 2 * PI;
-            area = radio * radio * PI;
+                System.out.print("Presiona ENTER para salir");
+                entrada.nextLine();
+            } else if (opcion.equals("2") || opcion.equals("b") || opcion.equals("B")) {
+                System.out.print("Ingresa el radio: ");
+                radio = entrada.nextDouble();
+                entrada.nextLine();
 
-            System.out.println("Perimetro: " + perimetro + "\nArea: " + area);
-        } else if (opcion.equals("3") || opcion.equals("c") || opcion.equals("C")) {
-            System.out.print("Ingresa la longitud: ");
-            longitud = entrada.nextDouble();
-            entrada.nextLine();
+                perimetro = radio * 2 * PI;
+                area = radio * radio * PI;
 
-            perimetro = longitud * 4;
-            area = longitud * longitud;
+                System.out.println("Perimetro: " + perimetro + "\nArea: " + area);
 
-            System.out.println("Perimetro: " + perimetro + "\nArea: " + area);
-        } else if (opcion.equals("4") || opcion.equals("d") || opcion.equals("D")) {
-            System.out.print("Ingresa la base: ");
-            base = entrada.nextDouble();
-            entrada.nextLine();
+                System.out.print("Presiona ENTER para salir");
+                entrada.nextLine();
+            } else if (opcion.equals("3") || opcion.equals("c") || opcion.equals("C")) {
+                System.out.print("Ingresa la longitud: ");
+                longitud = entrada.nextDouble();
+                entrada.nextLine();
 
-            System.out.print("Ingresa la altura: ");
-            altura = entrada.nextDouble();
-            entrada.nextLine();
+                perimetro = longitud * 4;
+                area = longitud * longitud;
 
-            System.out.print("Ingresa la longitud del primer lado: ");
-            double lado1 = entrada.nextDouble();
-            entrada.nextLine();
+                System.out.println("Perimetro: " + perimetro + "\nArea: " + area);
 
-            System.out.print("Ingresa la longitud del segundo lado: ");
-            double lado2 = entrada.nextDouble();
-            entrada.nextLine();
+                System.out.print("Presiona ENTER para salir");
+                entrada.nextLine();
+            } else if (opcion.equals("4") || opcion.equals("d") || opcion.equals("D")) {
+                System.out.print("Ingresa la base: ");
+                base = entrada.nextDouble();
+                entrada.nextLine();
 
-            System.out.print("Ingresa la longitud del tercer lado: ");
-            double lado3 = entrada.nextDouble();
-            entrada.nextLine();
+                System.out.print("Ingresa la altura: ");
+                altura = entrada.nextDouble();
+                entrada.nextLine();
 
-            area = (altura * base) / 2;
-            perimetro = lado1 + lado2 + lado3;
+                System.out.print("Ingresa la longitud del primer lado: ");
+                double lado1 = entrada.nextDouble();
+                entrada.nextLine();
 
-            System.out.println("Perimetro: " + perimetro + "\nArea: " + area);
-        } else if (opcion.equals("0") || opcion.equals("s") || opcion.equals("S")) {
-            System.out.print("Presione ENTER para salir");
-            entrada.nextLine();
-        } else {
-            System.out.println("ERROR");
+                System.out.print("Ingresa la longitud del segundo lado: ");
+                double lado2 = entrada.nextDouble();
+                entrada.nextLine();
+
+                System.out.print("Ingresa la longitud del tercer lado: ");
+                double lado3 = entrada.nextDouble();
+                entrada.nextLine();
+
+                area = (altura * base) / 2;
+                perimetro = lado1 + lado2 + lado3;
+
+                System.out.println("Perimetro: " + perimetro + "\nArea: " + area);
+
+                System.out.print("Presiona ENTER para salir");
+                entrada.nextLine();
+            } else {
+                System.out.println("ERROR");
+                System.out.print("Presione ENTER para salir");
+                entrada.nextLine();
+            }
+
+            //luego de escopger una opcion, el programa antes de finalizar te pide que vuelvas a ingresar una opcion
+            System.out.print("Escoge una opción: \n1 o a) Rectangulo \n2 o b) Circunferencia \n3 o c) Cuadrado \n4 o d) Triangulo \n0 o s) Para salir \n>> ");
+            opcion = entrada.nextLine();
         }
+
+        System.out.print("Presione ENTER para salir");
+        entrada.nextLine();
     }
 }
