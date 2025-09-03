59bc1aab149a5235cf7a11e7720eb9db47ec792e
ricarditodev <ricardodavid012@gmail.com>
2025-09-03 14:18:46 -0400

    version 2 del juego adivinador

--- src/com/ricarditodev/adivinadorV2/Main.java
+++ src/com/ricarditodev/adivinadorV2/Main.java
@@ -1,45 +1,49 @@
 package com.ricarditodev.adivinadorV2;
 
-import java.util.Random;
 import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in); //clase para leer datos de entrada
-        Random numeros = new Random(); //clase para generar numeros aleatorios
-        int numeroSecreto = numeros.nextInt(1000) + 1; //se genera un numero aleatorio entre 1 y 1000
+        Scanner entrada = new Scanner(System.in); //lectura de entrada
+        Random numeros = new Random(); //generar numeros aleatorios
 
-        final int MAX_INTENTOS = 10;
-        //valor ingresado por el usuario
-        int valorUsuario;
-        boolean acierto = false;
+        //genera numeros aleatorios entre 0 y 99 por eso sumamos 1
+        int numeroSecreto = numeros.nextInt(100) + 1;
+        //guardamos en una variable el numero ingresado por el usuario
+        int numeroLeido;
+        //si el usuario adivinó, cambiamos esta variable a true para controlar el flujo
+        boolean adivino = false;
 
-        //mostramos informacion en pantalla
+        final int MAX_INTENTOS = 10; //numero maximo de intentos
+
+        //mostramos al usuario el nombre del juego y los intentos que dispone para jugar
         System.out.println("Adivinador 2.0 – Dispones de 10 intentos para adivinar.");
-        System.out.println("Rango de valores: 1 a 1000 inclusive.\n");
 
+        //bucle que hace 10 intentos para que el usuario adivine el numero
         for (int i = 1; i <= MAX_INTENTOS; i++) {
-            System.out.print("Intento " + i + ">> ");
-            //leemos el dato ingresado por el usuario
-            valorUsuario = entrada.nextInt();
-            entrada.nextLine();
+            //mostramos el numero de intento al usuario
+            System.out.print("Intento " + i + ": ");
+            //pedimos que el usuario ingrese un numero
+            numeroLeido = entrada.nextInt();
 
-            if (i == 10) {
-                break;
-            } else if (valorUsuario == numeroSecreto) {
+            //evaluamos si el numero secreto coincide con el numero ingresado por el usuario
+            if (numeroSecreto == numeroLeido) {
                 System.out.println("¡¡¡GANASTE!!!");
-                acierto = true;
+                adivino = true; //si se cumple cambiamos adivino a true
                 break;
-            } else if (valorUsuario < numeroSecreto) {
-                System.out.println("• El número a adivinar es mayor");
-            } else if (valorUsuario > numeroSecreto) {
-                System.out.println("• El número a adivinar es menor");
+            } else if (i == MAX_INTENTOS) {
+                break;
+            } else if (numeroLeido < numeroSecreto) {
+                System.out.println("El número a adivinar es mayor");
+            } else if (numeroLeido > numeroSecreto) {
+                System.out.println("El número a adivinar es menor");
             }
         }
 
-        //si el usuario no acerto el numero, pierde y se muestra el numero secreto
-        if (!acierto) {
-            System.out.println("¡¡¡PERDISTE!!! El número era " + numeroSecreto);
+        //si al final el usuario no adivina el numero, negamos la variable que está en false para ejecutar el condicional
+        if (!adivino) {
+            System.out.print("¡¡¡PERDISTE!!! El numero era " + numeroSecreto);
         }
     }
 }
