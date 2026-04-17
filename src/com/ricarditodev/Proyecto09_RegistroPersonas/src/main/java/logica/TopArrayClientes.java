c7dd88f7784966712e467d14d57fce369d8b26ff
ricarditodev <ricardodavid012@gmail.com>
2026-04-17 19:08:32 -0400

    mejoras en la legibilÃidad del código

--- src/com/ricarditodev/Proyecto09_RegistroPersonas/src/main/java/logica/TopArrayClientes.java
+++ src/com/ricarditodev/Proyecto09_RegistroPersonas/src/main/java/logica/TopArrayClientes.java
@@ -11,9 +11,9 @@ public class TopArrayClientes {
     /**
      * Crea una lista vacía con una capacidad máxima igual a DEFAULT_ARRAY_LENGHT.
      */
-    public TopArrayClientes(){
-        this.listaClientes= new Cliente[Constantes.DEFAULT_ARRAY_LENGTH];
-        this.tope= -1;
+    public TopArrayClientes() {
+        this.listaClientes = new Cliente[Constantes.DEFAULT_ARRAY_LENGTH];
+        this.tope = -1;
     }
     
     /**
@@ -21,10 +21,10 @@ public class TopArrayClientes {
      * capacidad. Si el parámetro es 0 o menor que 0, se establecerá en DEFAULT_ARRAY_LENGTH.
      * @param capacidad La capacidad máxima a asignar a la lista.
      */
-    public TopArrayClientes(int capacidad){
-        capacidad= (capacidad<=0) ? Constantes.DEFAULT_ARRAY_LENGTH : capacidad;
-        this.listaClientes= new Cliente[capacidad];
-        this.tope= -1;
+    public TopArrayClientes(int capacidad) {
+        capacidad = (capacidad <= 0) ? Constantes.DEFAULT_ARRAY_LENGTH : capacidad;
+        this.listaClientes = new Cliente[capacidad];
+        this.tope = -1;
     }
     
     /**
@@ -35,14 +35,14 @@ public class TopArrayClientes {
      * @param capacidad La capacidad máxima que se asignará a la lista.
      * @param lista Una lista inicial de personas.
      */
-    public TopArrayClientes(int capacidad, Cliente... lista){
-        capacidad= (capacidad<lista.length) ? lista.length : capacidad;
-        this.listaClientes= new Cliente[capacidad];
-        this.tope= -1;
+    public TopArrayClientes(int capacidad, Cliente... lista) {
+        capacidad = (capacidad < lista.length) ? lista.length : capacidad;
+        this.listaClientes = new Cliente[capacidad];
+        this.tope = -1;
         
-        for(Cliente p: lista){
+        for(Cliente p: lista) {
             this.tope++;
-            this.listaClientes[this.tope]= p;
+            this.listaClientes[this.tope] = p;
         }
     }
     
@@ -51,9 +51,9 @@ public class TopArrayClientes {
      * <b>PRECONDICIÓN</b>: La lista no está llena {@link esLlena}
      * @param c La persona a insertar.
      */
-    public void insertar(Cliente c){
+    public void insertar(Cliente c) {
         this.tope++;
-        this.listaClientes[this.tope]= c;
+        this.listaClientes[this.tope] = c;
     }
     
     /**
@@ -61,7 +61,7 @@ public class TopArrayClientes {
      * <b>PRECONDICION</b>: La lista no está llena {@link esLlena}
      * @param c La persona a insetar.
      */
-    public void insertarInicio(Cliente c){
+    public void insertarInicio(Cliente c) {
         this.insertarEn(c, 0);
     }
     
@@ -73,21 +73,21 @@ public class TopArrayClientes {
      * @param c La persona a insertar.
      * @param indice La posición en que se insertará p.
      */
-    public void insertarEn(Cliente c, int indice){
-        Cliente aux[]= new Cliente[this.listaClientes.length-1-indice];
+    public void insertarEn(Cliente c, int indice) {
+        Cliente aux[] = new Cliente[this.listaClientes.length - 1 - indice];
         
-        int auxI=0;
-        for(int i= indice; i<=this.tope; i++){
-            aux[auxI]= this.listaClientes[i];
+        int auxI = 0;
+        for(int i = indice; i <= this.tope; i++){
+            aux[auxI] = this.listaClientes[i];
             auxI++;
         }
         
         this.tope++;
         
-        this.listaClientes[indice]= c;
-        auxI=0;
-        for(int i= indice+1; i<=this.tope; i++){
-            this.listaClientes[i]= aux[auxI];
+        this.listaClientes[indice] = c;
+        auxI =0 ;
+        for(int i = indice + 1; i <= this.tope; i++){
+            this.listaClientes[i] = aux[auxI];
             auxI++;
         }
     }
@@ -96,7 +96,7 @@ public class TopArrayClientes {
      * Indica la capacidad máxima de la lista.
      * @return La capacidad máxima de la lista.
      */
-    public int getCapacidad(){
+    public int getCapacidad() {
         return this.listaClientes.length;
     }
     
@@ -104,8 +104,8 @@ public class TopArrayClientes {
      * Indica la cantidad de personas almacenadas hasta el momento.
      * @return La cantidad de personas almacenadas.
      */
-    public int getCantidad(){
-        return this.tope+1;
+    public int getCantidad() {
+        return this.tope + 1;
     }
     
     /**
@@ -114,7 +114,7 @@ public class TopArrayClientes {
      * @param indice El índice de la persona buscada.
      * @return La persona en el índice indicado.
      */
-    public Cliente getCliente(int indice){
+    public Cliente getCliente(int indice) {
         return this.listaClientes[indice];
     }
     
@@ -124,24 +124,24 @@ public class TopArrayClientes {
      * @param indice El índice a validar.
      * @return TRUE si el índice es válido, FALSE si no lo es.
      */
-    public boolean esIndiceValido(int indice){
-        return (indice>=0) && (indice<=this.tope);
+    public boolean esIndiceValido(int indice) {
+        return (indice >= 0) && (indice <= this.tope);
     }
     
     /**
      * Indica si la lista está completa.
      * @return TRUE si está llena y ya no se pueden agregar personas, FALSE si no.
      */
-    public boolean esLlena(){
-        return this.tope==this.listaClientes.length-1;
+    public boolean esLlena() {
+        return this.tope == this.listaClientes.length -1;
     }
     
     /**
      * Indica si la lista está vacía.
      * @return TRUE si la lista está vacía, FALSE si no.
      */
-    public boolean esVacia(){
-        return this.tope==-1;
+    public boolean esVacia() {
+        return this.tope == -1;
     }
     
     /**
@@ -149,9 +149,9 @@ public class TopArrayClientes {
      * <b>PRECONDICIÓN</b>: La lista no está vacía {@link esVacia}.
      * @return La persona quitada.
      */
-    public Cliente quitarUltima(){
-        Cliente p= this.listaClientes[this.tope];
-        this.listaClientes[this.tope]= null;
+    public Cliente quitarUltima() {
+        Cliente p = this.listaClientes[this.tope];
+        this.listaClientes[this.tope] = null;
         this.tope--;
         return p;
     }
@@ -162,13 +162,14 @@ public class TopArrayClientes {
      * @param indice El índice desde el cual se quitará la persona.
      * @return La persona quitada.
      */
-    public Cliente quitarEn(int indice){
-        Cliente p= this.listaClientes[indice];
+    public Cliente quitarEn(int indice) {
+        Cliente p = this.listaClientes[indice];
         
-        for (int i=indice; i<this.tope; i++){
-            this.listaClientes[i]= this.listaClientes[i+1];
+        for (int i = indice; i < this.tope; i++){
+            this.listaClientes[i] = this.listaClientes[i + 1];
         }
-        this.listaClientes[this.tope]= null;
+
+        this.listaClientes[this.tope] = null;
         this.tope--;
         
         return p;
@@ -179,7 +180,7 @@ public class TopArrayClientes {
      * <b>PRECONDICIÓN:</b> La lista no está vacía {@link esVacia}.
      * @return  La persona quitada.
      */
-    public Cliente quitarPrimera(){
+    public Cliente quitarPrimera() {
         return this.quitarEn(0);
     }
 }
